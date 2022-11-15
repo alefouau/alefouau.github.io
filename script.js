2340a43da4cb98d9561c55186338763e23c50f87
Alefo <56359523+alefouau@users.noreply.github.com>
2022-11-15 20:09:56 -0300

    versão 2
    refiz o site do zero novamente

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-
-function goto(x){
-    var tabContent = document.getElementsByClassName('tab_content'); // isso retorna um array com todos os elementos com a classe "tab_content"
-    var tabBtn = document.getElementsByClassName('tab_btn');
-    //ocuta todos os elementos da classe tabcontent e exibe a aba selecionada (x)
-    for (i = 0; i < tabContent.length ; i++){
-	tabContent[i].opacity = "0.0";
-        tabContent[i].style.display = "none";
-        tabBtn[i].id = "inactive";
+let tab_content = document.querySelectorAll("tab-content"); /*pega a lista com todas as abas*/
+let tab_btn = document.querySelectorAll("nav>button"); /*pega todos os botoes de aba*/
+let banner_title = document.querySelector("banner-text>h1");
+let banner_subtitle = document.querySelector("banner-text>h2");
+let banner_img = document.querySelector("header>img");
+tab(0);
+function tab(sel){
+    for(i = 0 ; i < tab_content.length; i++){
+        tab_content[i].style.display = "none";
+        tab_btn[i].id = "";
     }
-    tabContent[x].style.display = "block";
-    tabContent[x].opacity = "1.0";
-    tabBtn[x].id = "active";
+    banner_title.innerText = tab_content[sel].getAttribute("tab-title");
+    banner_subtitle.innerText = tab_content[sel].getAttribute("tab-desc");
+    banner_img.src = tab_content[sel].getAttribute("banner-image");
+    tab_content[sel].style.display = "flex";
+    tab_btn[sel].id = "active";
 }
